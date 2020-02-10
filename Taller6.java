bfb06153882e904c7b86fc9d2292ccbdf8311d26
SebasQuiYeminus <sebasqui1995@gmail.com>
2020-02-10 16:02:33 -0500

    Taller V - Programación  Orientada a Objetos - Java

---
+++ Taller6.java
@@ -0,0 +1,13 @@
+package taller6;
+import java.util.ArrayList;
+public class Taller6 {
+
+    public static void main(String[] args) {
+      Menu menu = new Menu();
+      menu.setTitle("TALLER 6");
+      menu.setVisible(true);
+     
+        
+    }
+    
+}
